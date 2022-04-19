ab694273503b70fe229e0e562ccfcf3c1e2c242e
varbinsoftwares <varbinsoftwares@gmail.com>
2022-04-19 19:15:11 +0530

    ad fils

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,4 @@
 String mainsite = "https://ae.varbin.com";
 //String mainsite = "http://192.168.3.2/allianceadmin/";
 String apiendpoint = mainsite + "/index.php/LoyaltyApi";
-
-
 String ecom_apiendpoint = mainsite + "/index.php/Api";
